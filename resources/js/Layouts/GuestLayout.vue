31fddb1b1db193f5194a1ba719babe7df02eeb33
Marcos_MEDENOU <mmedenou@vippinterstis.com>
2023-11-15 09:22:31 +0100

    GuestLayout updated

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -13,9 +13,9 @@ import { Link } from '@inertiajs/vue3';
         >
         <div class="flex flex-col items-center justify-center my-3 ">
             <Link href="/">
-                <ApplicationLogo class="w-32 h-10 text-gray-500 fill-current" />
+                <ApplicationLogo class="w-auto h-10 text-gray-500 fill-current" />
             </Link>
-            <div class="ml-6 text-xs font-bold text-gray-500 text-bold-800">DASHBOARD</div>
+            <div class="text-xs font-bold text-gray-500  text-bold-800">DASHBOARD</div>
         </div>
             <slot />
         </div>
