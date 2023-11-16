60e6344d00884fa945abcd240302e6405be6ab1a
Lejeunesage <geoffroyotegbeye@gmail.com>
2023-11-16 13:30:14 +0100

    Page post mise à jour. reste le back

---
+++ resources/js/Pages/Front-end/Partials/AsideRight.vue
@@ -0,0 +1,81 @@
+<script setup>
+import axios from 'axios';
+import { onMounted, reactive } from 'vue';
+import { router } from '@inertiajs/vue3'
+
+const data = reactive({
+    recentPosts: [],
+});
+
+
+const newArticles = [
+    {
+        category: 'High-Tech',
+        title: 'Lorem Ipsum Dolor',
+        content: 'Lorem ipsum dolor lodicum opazalin dhiloh...',
+        image: 'https://res.cloudinary.com/muhammederdem/image/upload/q_60/v1535759872/kuldar-kalvik-799168-unsplash.webp',
+    },
+    {
+        category: 'Immobilier',
+        title: 'Lorem Ipsum Dolor2',
+        content: 'Lorem ipsum dolor lodicum opazalin dhiloh...',
+        image: 'https://res.cloudinary.com/muhammederdem/image/upload/q_60/v1535759871/jason-leung-798979-unsplash.webp',
+    },
+    {
+        category: 'Société',
+        title: 'Lorem Ipsum Dolor',
+        content: 'Lorem ipsum dolor lodicum opazalin dhiloh...',
+        image: 'https://res.cloudinary.com/muhammederdem/image/upload/q_60/v1535759871/alessandro-capuzzi-799180-unsplash.webp',
+    },
+];
+
+
+</script>
+
+
+<template>
+    <section class="py-10 lg:ml-24 sticky top-20">
+        <!-- Articles liés -->
+        <h1 class="text-2xl font-bold mb-2">Derniers Articles</h1>
+        <div class="my-4">
+
+            <div v-for="(item, index) in newArticles" :key="index"
+                class="flex gap-2 mb-3 bg-white rounded-md shadow-xl p-5 relative h-32">
+                <div class="absolute -left-6 shadow-xl top-4">
+                    <img :src="item.image" alt="" class="h-24 w-24 rounded-lg">
+                </div>
+                <div class="ml-16">
+                    <span class="italic">-{{ item.category }}-</span>
+                    <div class="font-semibold">{{ item.title }}</div>
+                    <div class="font-">{{ item.content }}</div>
+
+                    <div
+                        class="absolute bottom-1 right-4 px-3 py-1  font-medium text-white bg-[#e39a00]  rounded-lg">
+                        <a href="#" class="">Lire plus</a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+        <!-- Formulaire d'abonnement à la newsletter -->
+        <div class="mt-10">
+            <h3 class="text-2xl font-bold mb-2">Newsletter</h3>
+            <p class="text-gray-700 mb-2">Abonnez-vous à notre newsletter pour les dernières mises à jour.</p>
+            <form action="#" method="post">
+                <input
+                    class="block p-3  w-full  text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 mb-2"
+                    type="email" name="email" placeholder="Votre adresse e-mail" id="email" required="">
+
+                <button type="submit"
+                    class="bg-green-500 hover:bg-green-600 rounded-lg text-white p-2 w-full">S'abonner</button>
+                <p class="mt-2">Nous nous soucions de la protection de vos données. Lisez notre <a
+                        class="text-blue-900 underline" href="/">politique de
+                        confidentialité</a>.</p>
+            </form>
+        </div>
+    </section>
+</template>
+<style scope>
+@import url('https://fonts.googleapis.com/css?family=Fira+Sans:400,500,600,700,800');
+</style>
--- resources/js/Pages/Post.vue
+++ resources/js/Pages/Post.vue
@@ -2,8 +2,8 @@
 import axios from 'axios';
 import { computed, ref, onMounted } from 'vue'
 import MainLayout from './Front-end/Layouts/MainLayout.vue';
-import Hero from './Front-end/Partials/Hero.vue';
-import Articles from './Front-end/Partials/Articles.vue';
+import AsideRight from './Front-end/Partials/AsideRight.vue';
+import Comment from './Front-end/Partials/Comment.vue';
 const props = defineProps({
     post: {
         type: Object,
@@ -29,42 +29,136 @@ const props = defineProps({
                 {{ props.post['category'].name }}
             </span>
         </div>
+        <div class=" flex justify-center mt-8">
 
-        <div class="container mx-auto my-8 w-[800px]">
-            <div class="flex items-center justify-between pb-4 border-b">
-                <div class="text-gray-700">
-                    <!-- <p class="text-lg font-semibold">Auteur : {{ props.post['author'].name }}</p> -->
-                    <p class="text-lg font-semibold">Catégorie : {{ props.post['category'].name }}</p>
+
+            <div class="w-6/12 lg:w-6/12 xl:w-6/12 ">
+                <div class="flex items-center justify-between pb-4 border-b">
+                    <div class="text-gray-700">
+                        <!-- <p class="text-lg font-semibold">Auteur : {{ props.post['author'].name }}</p> -->
+                        <p class="text-lg font-semibold">Catégorie : {{ props.post['category'].name }}</p>
+                    </div>
+                    <div class="text-gray-700">
+                        <p class="text-lg font-semibold">Publié le {{ props.post['created_at'] }}</p>
+                        <p class="text-lg font-semibold">Dernière mise à jour le {{ props.post['updated_at'] }}</p>
+                    </div>
                 </div>
-                <div class="text-gray-700">
-                    <p class="text-lg font-semibold">Publié le {{ props.post['created_at'] }}</p>
-                    <p class="text-lg font-semibold">Dernière mise à jour le {{ props.post['updated_at'] }}</p>
+
+                <div class="mt-6 mb-12">
+                    <h1 class="mb-4 text-5xl font-bold text-[#e39a00]">{{ props.post.title }}</h1>
+                    <div class="prose">
+                        <div v-html="props.post['content']"></div>
+                    </div>
+                    <div class="flex items-center space-x-4 mt-5">
+                        <!-- Bouton Like -->
+                        <button class="flex items-center text-green-500 hover:text-green-700">
+                            <Icon name="like" />
+                            <span class="ml-2">J'aime(0)</span>
+                        </button>
+
+                        <!-- Bouton Dislike -->
+                        <button class="flex items-center text-red-500 hover:text-red-700">
+                            <Icon name="dislike" />
+                            <span class="ml-2">J'aime pas(0)</span>
+                        </button>
+                    </div>
                 </div>
+
+                <div class="flex items-center justify-between border-t border-gray-300 pt-8">
+                    <div class="text-gray-700 cursor-pointer  ">
+                        <span
+                            class=" p-5 rounded-lg shadow-md bg-green-500 font-bold text-lg hover:scale-50 hover:bg-green-600 text-white">
+                            Article précédent</span>
+                        <span class="block text-lg font-bold mt-8">Titre de l'article précédent</span>
+                    </div>
+                    <div class="text-gray-700 cursor-pointer  ">
+                        <span
+                            class=" p-5 mb-5 rounded-md shadow-lg bg-green-500 font-bold text-lg hover:scale-50 hover:bg-green-600 text-white">
+                            Article suivant</span>
+                        <span class="block text-lg font-bold mt-8">Titre de l'article suivant</span>
+                    </div>
+                </div>
+
+                <!-- Commentaires -->
+                <Comment />
+
+
+
             </div>
 
-            <div class="mt-6 mb-12">
-                <h1 class="mb-4 text-5xl font-bold text-[#e39a00]">{{ props.post.title }}</h1>
-                <div class="prose">
-                    <div v-html="props.post['content']"></div>
-                </div>
-            </div>
-
-            <div class="flex items-center justify-between border-t border-gray-300 pt-8">
-                <div class="text-gray-700 cursor-pointer  ">
-                    <span class=" p-5 rounded-lg shadow-md bg-green-500 font-bold text-lg hover:scale-50 hover:bg-green-600 text-white"> Article précédent</span>
-                    <span class="block text-lg font-bold mt-8">Titre de l'article précédent</span>
-                </div>
-                <div class="text-gray-700 cursor-pointer  ">
-                    <span class=" p-5 mb-5 rounded-md shadow-lg bg-green-500 font-bold text-lg hover:scale-50 hover:bg-green-600 text-white"> Article suivant</span>
-                    <span class="block text-lg font-bold mt-8">Titre de l'article suivant</span>
-                </div>
+            <div class="w-3/12 lg:w-3/12 xl:w-3/12 ">
+                <AsideRight />
             </div>
 
         </div>
+        <div  class="py-8 lg:py-24  dark:bg-gray-800">
+            <div class="px-4 mx-auto max-w-screen-xl">
+                <h2 class="mb-8 text-2xl font-bold text-gray-900 dark:text-white">Articles Liés</h2>
+                <div class="grid gap-5 sm:grid-cols-2 lg:grid-cols-4">
+                    <article class="max-w-xs bg-white p-4 rounded-lg shadow-lg">
+                        <a href="#">
+                            <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/article/blog-1.png"
+                                class="mb-5 rounded-lg" alt="Image 1">
+                        </a>
+                        <h2 class="mb-2 text-xl font-bold leading-tight text-gray-900 dark:text-white">
+                            <a href="#">Our first office</a>
+                        </h2>
+                        <p class="mb-4 text-gray-500 dark:text-gray-400">Over the past year, Volosoft has undergone many
+                            changes! After months of preparation.</p>
+                        <a href="#"
+                            class="inline-flex items-center font-medium underline underline-offset-4 text-primary-600 dark:text-primary-500 hover:no-underline">
+                            Lire en 2 minutes
+                        </a>
+                    </article>
+                    <article class="max-w-xs bg-white p-4 rounded-lg shadow-lg">
+                        <a href="#">
+                            <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/article/blog-2.png"
+                                class="mb-5 rounded-lg" alt="Image 2">
+                        </a>
+                        <h2 class="mb-2 text-xl font-bold leading-tight text-gray-900 dark:text-white">
+                            <a href="#">Enterprise design tips</a>
+                        </h2>
+                        <p class="mb-4  text-gray-500 dark:text-gray-400">Over the past year, Volosoft has undergone many
+                            changes! After months of preparation.</p>
+                        <a href="#"
+                            class="inline-flex items-center font-medium underline underline-offset-4 text-primary-600 dark:text-primary-500 hover:no-underline">
+                            Lire en 12 minutes
+                        </a>
+                    </article>
+                    <article class="max-w-xs bg-white p-4 rounded-lg shadow-lg">
+                        <a href="#">
+                            <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/article/blog-3.png"
+                                class="mb-5 rounded-lg" alt="Image 3">
+                        </a>
+                        <h2 class="mb-2 text-xl font-bold leading-tight text-gray-900 dark:text-white">
+                            <a href="#">We partnered with Google</a>
+                        </h2>
+                        <p class="mb-4  text-gray-500 dark:text-gray-400">Over the past year, Volosoft has undergone many
+                            changes! After months of preparation.</p>
+                        <a href="#"
+                            class="inline-flex items-center font-medium underline underline-offset-4 text-primary-600 dark:text-primary-500 hover:no-underline">
+                            Lire en 8 minutes
+                        </a>
+                    </article>
+                    <article class="max-w-xs bg-white p-2 rounded-lg shadow-lg">
+                        <a href="#">
+                            <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/article/blog-4.png"
+                                class="mb-5 rounded-lg" alt="Image 4">
+                        </a>
+                        <h2 class="mb-2 text-xl font-bold leading-tight text-gray-900 dark:text-white">
+                            <a href="#">Our first project with React</a>
+                        </h2>
+                        <p class="mb-4  text-gray-500 dark:text-gray-400">Over the past year, Volosoft has undergone many
+                            changes! After months of preparation.</p>
+                        <a href="#"
+                            class="inline-flex items-center font-medium underline underline-offset-4 text-primary-600 dark:text-primary-500 hover:no-underline">
+                            Lire en 4 minutes
+                    </a>
+                </article>
+            </div>
+        </div>
+    </div>
 
 
-
-
-    </MainLayout>
-</template>
+</MainLayout></template>
 
